f2d0770bdcadc41c13cab720c8760ff76db2c501
Tetiana Sonchak <tetiana.sonchak@gmail.com>
2022-12-26 19:03:20 +0200

    Commit Test_Flask

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello")
+import datetime
+import random
+import string
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!!!!</p>"
+
+
+@app.route("/now")
+def now():  # view function
+    return str(datetime.datetime.now())
+
+
+@app.route('/random')
+def get_random():  # view function
+    length = int(request.args['length'])
+    result = ''
+    for i in range(length):
+        result += random.choice(string.ascii_lowercase)
+    return result
+
+
+app.run()
